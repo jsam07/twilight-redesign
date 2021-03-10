8a48c4315c4b476d4d55899eb49ca494b63457f4
jfoxx77 <jsam07@hotmail.com>
2021-03-09 19:13:44 -0800

    Added movie tags (meta info about movie)

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,21 @@
 $(document).ready(() => {
-    // Check for click events on the navbar burger icon
+    // Dynamically Set Hero Section Height
+    function setHeroSectionHeight() {
+        const viewportHeight = $(window).height();
+        const navbarHeight = $('.navbar').height();
+        const heroSectionHeight = viewportHeight - navbarHeight;
+
+        $('#hero-poster-container').height(heroSectionHeight);
+    }
+
+    $(window).resize(() => {
+        setHeroSectionHeight();
+    });
+
+    setHeroSectionHeight();
+
+    // Expand Hamburger Menu
     $('.navbar-burger').click(() => {
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         $('.navbar-burger').toggleClass('is-active');
         $('.navbar-menu').toggleClass('is-active');
     });
@@ -38,6 +52,7 @@ $(document).ready(() => {
         });
     }
 
+    // Modal Event Listeners
     playTrailerBtn.on('click', () => {
         resetTrailer();
         disableScrolling();
